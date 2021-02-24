c65bfa6b11e2a1306e68395bd4a2801ec8902960
ruzhuang <ruzhuang@ucsd.edu>
2021-02-24 15:29:31 +0800

    loading effect static

--- script_instruction.js
+++ script_instruction.js
@@ -1,43 +1,46 @@
-if(localStorage.getItem("show")=="false" && localStorage.getItem("forceOpen")==null){
-    window.href="game.html";
+if (localStorage.getItem("show") == "false" && localStorage.getItem("forceOpen") == null) {
+    window.href = "game.html";
 }
 
 localStorage.removeItem("forceOpen")
-document.onreadystatechange = function () {
-    var state = document.readyState
-    if (state == 'interactive') {
-        console.log("here")
-         document.getElementById('text-box').style.display="hidden";
-         document.getElementById('text-box').style.overflow="hidden";
-    } else if (state == 'complete') {
-           document.getElementById('interactive');
-           document.getElementById('load').style.visibility="hidden";
-           document.getElementById('text-box').style.visibility="visible";
-           document.getElementById('text-box').style.overflow="scroll";
+var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
 
+[].forEach.call(imgs, function(img) {
+    if (img.complete)
+        incrementCounter();
+    else
+        img.addEventListener('load', incrementCounter, false);
+});
+
+function incrementCounter() {
+    counter++;
+    if (counter === len) {
+        document.getElementById("load").style.display = "none";
     }
-  }
+}
 const button = document.getElementById("skip");
-button.addEventListener("click",skip);
-function skip(){
-    location.href="game.html";
+button.addEventListener("click", skip);
+
+function skip() {
+    location.href = "game.html";
 }
 
 const changeShow = document.getElementById("do-not-show-button");
-if(localStorage.getItem("show")=="false"){
-    changeShow.innerHTML="Show this instruction on start of game";
+if (localStorage.getItem("show") == "false") {
+    changeShow.innerHTML = "Show this instruction on start of game";
+} else {
+    changeShow.innerHTML = "Stop showing this instruction on start of game"
 }
-else{
-    changeShow.innerHTML="Stop showing this instruction on start of game"
-}
-changeShow.addEventListener("click",changeShowing);
-function changeShowing(){
-    if(localStorage.getItem("show")=="true"){
-        localStorage.setItem("show","false");
-        changeShow.innerHTML="Show this instruction on start of game";
-    }
-    else{
-        localStorage.setItem("show","true");
-        changeShow.innerHTML="Stop showing this instruction on start of game";
+changeShow.addEventListener("click", changeShowing);
+
+function changeShowing() {
+    if (localStorage.getItem("show") == "true") {
+        localStorage.setItem("show", "false");
+        changeShow.innerHTML = "Show this instruction on start of game";
+    } else {
+        localStorage.setItem("show", "true");
+        changeShow.innerHTML = "Stop showing this instruction on start of game";
     }
 }
